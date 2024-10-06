c059848de9e9c045a1b720fdf86e8c04aa284b82
cherrydub <cherrydub@gmail.com>
2024-10-06 17:31:29 +0100

    refactor: responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home () {
   return (
-    <div className='flex-center flex-col sm:flex-row h-[calc(100dvh-80px)] gap-2 cursor-default grayscale hover:grayscale-0'>
+    <div className='flex-center flex-col sm:flex-row h-[calc(100dvh-60px)] gap-2 cursor-default grayscale hover:grayscale-0'>
       <span className='text-7xl'>💾</span>
       <span className='text-7xl'>👾</span>
       <span className='text-7xl'>💿</span>
+      <span className='text-7xl'>🌐</span>
     </div>
   )
 }
