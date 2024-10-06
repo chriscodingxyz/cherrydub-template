8957f4172b5f5400bce54a8c475ef2692502497b
cherrydub <cherrydub@gmail.com>
2024-10-06 17:16:06 +0100

    perf: add container center auto

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from './ThemeToggle'
 export default function Header () {
   return (
     <div className='w-screen border-b sticky top-0 z-101 bg-background'>
-      <div className='mx-auto container justify-between flex items-center w-screen px-4'>
+      <div className=' container justify-between flex items-center w-screen'>
         <span>cherrydub 🍒</span>
         <ThemeToggle />
       </div>
